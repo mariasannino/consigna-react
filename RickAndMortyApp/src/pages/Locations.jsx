3d631cca6115468e6e9a820ccce07cf260dbf28e
mariasannino <maria.sannino01@estudiant.upf.edu>
2025-05-29 15:09:47 +0200

    feat: fix search

--- RickAndMortyApp/src/pages/Locations.jsx
+++ RickAndMortyApp/src/pages/Locations.jsx
@@ -1,22 +1,24 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getLocationByPage, searchLocations } from "../store/locationsSlice";
+import { filterLocations, getLocationByPage } from "../store/locationsSlice";
 import Location from "../components/location/Location";
 import sad from "../assets/sad.png";
 import Filters from "../components/filters/Filters";
 const Locations = () => {
   const dispatch = useDispatch();
-  const { locations, pageNum, searchQuery } = useSelector(
+  const { locations, pageNum, searchQuery, type, dimension } = useSelector(
     (state) => state.locations
   );
 
   useEffect(() => {
-    if (searchQuery.trim() !== "") {
-      dispatch(searchLocations({ name: searchQuery, pageNum }));
+    if (searchQuery.trim() !== "" || type || dimension) {
+      dispatch(
+        filterLocations({ name: searchQuery, type, dimension, pageNum: 1 })
+      );
     } else {
       dispatch(getLocationByPage(pageNum));
     }
-  }, [dispatch, pageNum, searchQuery]);
+  }, [dispatch, pageNum, searchQuery, dimension, type]);
 
   if (locations.length === 0) {
     return (
